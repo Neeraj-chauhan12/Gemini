2de3a4803db24d1cfa3d28acf11ff6ff3c720f9d
Neeraj chauhan <nc1676639@gmail.com>
2025-12-20 09:04:12 +0530

    deploy the website

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const userRoute = require("./src/routes/userRoute");
 const chatRoute = require("./src/routes/chatRoute");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path=require('path')
 
 const app = express();
 dotenv.config();
@@ -20,11 +21,19 @@ app.use(
   })
 );
 
+const _dirname=path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/user", userRoute);
 app.use("/api/chat", chatRoute);
+
+// deploy code
+app.use(express.static(path.join(_dirname, '/client/dist')));
+app.get((req,res)=>{
+  res.sendFile(path.resolve(_dirname,'/client/dist/index.html'))
+})
 
 mdConnection();
 const PORT = process.env.PORT || 4000;
